37b75593bb1371a7b3052857c752d337d67fcba2
Tanmay Samdani <tanmaysamdani12345@gmail.com>
2023-02-03 00:49:05 +0530

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,25 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currentURL) {
+  // fetch and parse the html of the currentURL
+  console.log(`crawling ${currentURL}`);
+  try {
+    const resp = await fetch(currentURL);
+    if (resp.status > 399) {
+      console.log(`Got HTTP error, status code: ${resp.status}`);
+      return;
+    }
+    const contentType = resp.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(`Got non-html response: ${contentType}`);
+      return;
+    }
+    console.log(await resp.text());
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -22,16 +42,17 @@ function getURLsFromHTML(htmlBody, baseURL) {
   return urls;
 }
 
-function normalizeURL(urlString) {
-  const urlObj = new URL(urlString);
-  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
-  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
-    return hostPath.slice(0, -1);
+function normalizeURL(url) {
+  const urlObj = new URL(url);
+  let fullPath = `${urlObj.host}${urlObj.pathname}`;
+  if (fullPath.length > 0 && fullPath.slice(-1) === "/") {
+    fullPath = fullPath.slice(0, -1);
   }
-  return hostPath;
+  return fullPath;
 }
 
 module.exports = {
+  crawlPage,
   normalizeURL,
   getURLsFromHTML,
 };
